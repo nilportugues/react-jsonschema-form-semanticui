e0d5d6b1b4082027b9030f125fe795b94275ea79
Nil Portugués Calderó <nilopc@gmail.com>
2017-11-14 22:48:31 +0100

    Radio button group

--- src/components/widgets/RadioWidget.js
+++ src/components/widgets/RadioWidget.js
@@ -1,52 +1,87 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Container, Checkbox } from "semantic-ui-react";
 
-function RadioWidget(props) {
-  const {
-    options,
-    value,
-    required,
-    disabled,
-    readonly,
-    autofocus,
-    onChange,
-  } = props;
-  // Generating a unique field name to identify this set of radio buttons
-  const name = Math.random().toString();
-  const { enumOptions, inline } = options;
-  // checked={checked} has been moved above name={name}, As mentioned in #349;
-  // this is a temporary fix for radio button rendering bug in React, facebook/react#7630.
-  return (
-    <div className="field-radio-group">
-      {enumOptions.map((option, i) => {
-        const checked = option.value === value;
-        const disabledCls = disabled || readonly ? "disabled" : "";
+class RadioGroup extends Component {
+  render() {
+    const name = Math.random().toString();
+    const { enumOptions, inline } = this.props.options;
+
+    {
+      enumOptions.map((option, i) => {
+        const checked = option.value === this.props.value;
+        const disabledCls =
+          this.props.disabled || this.props.readonly ? "disabled" : "";
         const radio = (
-          <span>
-            <input
-              type="radio"
-              checked={checked}
-              name={name}
-              required={required}
-              value={option.value}
-              disabled={disabled || readonly}
-              autoFocus={autofocus && i === 0}
-              onChange={_ => onChange(option.value)}
-            />
-            <span>{option.label}</span>
-          </span>
+          <Checkbox
+            radio
+            checked={checked}
+            name={name}
+            required={this.props.required}
+            value={option.value}
+            disabled={this.props.disabled || this.props.readonly}
+            autoFocus={this.props.autofocus && i === 0}
+            onChange={_ => this.props.onChange(option.value)}
+            label={option.label}
+          />
         );
 
-        return inline ? (
-          <label key={i} className={`radio-inline ${disabledCls}`}>
-            {radio}
-          </label>
-        ) : (
+        if (inline) {
+          return (
+            <label key={i} className={`radio-inline ${disabledCls}`}>
+              {radio}
+            </label>
+          );
+        }
+        return (
           <div key={i} className={`radio ${disabledCls}`}>
             <label>{radio}</label>
           </div>
         );
-      })}
+      });
+    }
+
+    return (
+      <Container>
+        {enumOptions.map((option, i) => {
+          const checked = option.value === this.props.value;
+          const disabledCls =
+            this.props.disabled || this.props.readonly ? "disabled" : "";
+          const radio = (
+            <span>
+              <Checkbox
+                type="radio"
+                checked={checked}
+                name={name}
+                required={this.props.required}
+                value={option.value}
+                disabled={this.props.disabled || this.props.readonly}
+                autoFocus={this.props.autofocus && i === 0}
+                onChange={_ => this.props.onChange(option.value)}
+                label={option.label}
+              />
+            </span>
+          );
+
+          return inline ? (
+            <label key={i} className={`radio-inline ${disabledCls}`}>
+              {radio}
+            </label>
+          ) : (
+            <div key={i} className={`radio ${disabledCls}`}>
+              <label>{radio}</label>
+            </div>
+          );
+        })}
+      </Container>
+    );
+  }
+}
+
+function RadioWidget(props) {
+  return (
+    <div style={{ display: "inline-block" }}>
+      <RadioGroup {...props} />
     </div>
   );
 }
