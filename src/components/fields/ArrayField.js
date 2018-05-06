4e1d9e850bbf26d644386715d0b33ee8ee17c260
Nil Portugués Calderó <nil@trovit.com>
2018-05-06 22:10:42 +0200

    package upgrades

--- src/components/fields/ArrayField.js
+++ src/components/fields/ArrayField.js
@@ -602,7 +602,9 @@ class ArrayField extends Component {
       uiSchema,
       registry = getDefaultRegistry(),
     } = this.props;
-    const { fields: { SchemaField } } = registry;
+    const {
+      fields: { SchemaField },
+    } = registry;
     const { orderable, removable } = {
       orderable: true,
       removable: true,
